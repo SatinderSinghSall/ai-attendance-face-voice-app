ed4ce641df366f5d97d34da9a4f5de9244682029
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-22 15:43:15 +0530

    Intelligent AI Attendance - Face & Voice app deployment configarations set.

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,8 @@ import io
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
 
-    app_domain = "http://localhost:8501"
+    # app_domain = "http://localhost:8501"
+    app_domain = "https://ai-attendance-app.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
